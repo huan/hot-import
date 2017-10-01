d9c84ba04ad6122c377438b094871a65390903c5
Huan LI <zixia@zixia.net>
2017-10-02 01:55:05 +0800

    enable export local log

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import {
+  log,
+}                 from 'brolog'
+import {
   callerResolve,
   hotImport,
-}             from './src/hot-import'
+}                 from './src/hot-import'
 
 export {
   callerResolve,
   hotImport,
+  log,
 }
 export default hotImport
